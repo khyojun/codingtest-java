872ef6be388bb54fc805c106b88deb1da0f6e985
Khyojun <38220795+khyojun@users.noreply.github.com>
2025-01-24 16:27:40 +0900

    [level 1] Title: 둘만의 암호, Time: 0.86 ms, Memory: 14.1 MB -BaekjoonHub

--- Ruby/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.rb
+++ Ruby/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.rb
@@ -4,22 +4,21 @@ def getNextChar(s, skip, index)
     skipCnt=0
     # index 만큼 카운트가 되면 탈출이고 계속 넘어가게 되는 경우는 없음 skip 이 10개이기 때문에 걱정 ㄴㄴ
     # 숫자를 더해야함! -> 어떤 숫자? 지금부터 지나간 index만큼을 기록하는 친구가 필요함!
-    while 1==1 do   
-        # 오류의 경우 지금 바로 다음게 있는데 -> 다음번 더했는데 예를들어서 이 친구가 스킵해야하면 스킵해야하는데
+    while 1==1 do
+        
         if s.ord>'z'.ord
-            s = (s.ord - 'z'.ord + 'a'.ord-1).chr
+            s = 'a'
         end
-
-        if skip.include?((s.ord).chr)
-            s= (s.ord+1).chr
-            skipCnt+=1
-        elsif cnt>=index
+        
+        if cnt>=index
             return s
             break
+        elsif skip.include?(s)
+            s= (s.ord+1).chr
+            skipCnt+=1
         else
             s= (s.ord+1).chr
             cnt+=1
-            
         end
     end
 end
@@ -35,8 +34,8 @@ def solution(s, skip, index)
     # 이거에 대한 판단을 무한 루프를 통해 계속 갈때까지 봐야할 케이스도 있음!
     # 위 역할에 대한 함수 따로 분리해서 진행! 뭘 도대체 더할 수 있는지!
     
-    return s.chars.map{|t|
-        getNextChar(t, skip, index)
+    return s.chars.map{ |t|
+         plusIndex = getNextChar(t, skip, index)
     }.join
     
     
