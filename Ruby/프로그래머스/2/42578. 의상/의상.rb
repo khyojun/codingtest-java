1baa0618e00c705c4b4cd4494af5e15f2bba4fe1
Khyojun <38220795+khyojun@users.noreply.github.com>
2025-02-02 13:39:00 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 14.1 MB -BaekjoonHub

--- Ruby/프로그래머스/2/42578. 의상/의상.rb
+++ Ruby/프로그래머스/2/42578. 의상/의상.rb
@@ -1,29 +1,4 @@
 def solution(clothes)
-    answer = 1
-    
-    hash=clothes.to_h
-    tal=hash.map{|k,v| v}.tally
-    puts tal
-
-    #abc + ab+ ac+ bc + a+ b+ c
-    
-    
-    # 이거 공식으로 하면 조합론의 공식이 되어진다! 그래서 
-    
-    
-    
-    
-    # (a+1)(b+1)(c+1) 이 되어서 이렇게 된다.
-    
-    
-    tal.each{
-        |k,v|
-        answer*=(v+1)
-    }
-    
-    
-    
-    
-    
-    return answer-1
+    answer = 0
+    return answer
 end
