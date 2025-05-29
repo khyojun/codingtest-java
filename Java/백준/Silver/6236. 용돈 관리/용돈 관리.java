0cc13f2e69641a324bcfc66c01add11cac9c0e70
khyojun <38220795+khyojun@users.noreply.github.com>
2025-05-29 20:37:38 +0900

    [Silver I] Title: 용돈 관리, Time: 288 ms, Memory: 25488 KB -BaekjoonHub

---
+++ Java/백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+import java.lang.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+        int sum =0;
+        for(int i=0; i<n; i++){
+            list.add(Integer.parseInt(br.readLine()));
+            sum += list.get(i);
+        }
+
+        int min = Collections.max(list);
+        int max = sum;
+
+        int s = min;
+        int e = max;
+        int result =0;
+
+        // 5 3
+        // 1 2 3 4 5
+        // 5 15
+        while(s<=e){
+            int cnt=1;
+            int mid = (s+e)/2; // 이게 돈
+            int total =mid;
+
+            for(int i=0; i<n; i++){
+                if(total- list.get(i)<0){
+                    total=mid-list.get(i);
+                    cnt++;
+                }
+                else{
+                    total -=list.get(i);
+                }
+            }
+            // 나가긴 하지만 -> 최소 금액을 맞추기 위해 어떤 조건일 때 그렇게 하는게 좋을까?
+            //
+
+            if(cnt>m){
+                s= mid+1;
+            }else{
+                result= e;
+                e= mid-1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
